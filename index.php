8be700396554d982ca97d93e0f39e310b735da9f
Nicole <gingrasnic@gmail.com>
2015-03-26 11:10:27 -0400

    1er commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="travail pratique">
+    <meta name="author" content="nicole gingras">
+
+    <title>TP2 - Nicole Gingras</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+</head>
+<body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Travail pratique</a>
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Accueil</a></li>
+                    <li><a href="#about">&Agrave; propos</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+
+    <!-- jumbotron -->
+    <div class="jumbotron clearfix">
+        <div class="container">
+            <h1>Mon travail pratique!</h1>
+            <p>Ce travail a &eacute;t&eacute; r&eacute;alis&eacute; dans le cadre du cours "Entretien et rayonnement d'un site web". Cette page a &eacute;t&eacute; cr&eacute;&eacute;e &agrave; partir d'un mod&egrave;le.</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <!-- Example row of columns -->
+        <div class="row">
+            <div class="col-md-8">
+                <h2>Buts</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab deleniti eligendi facilis illo incidunt, modi voluptate. Blanditiis culpa, cumque nostrum quam quas tenetur. Itaque nam, neque nesciunt repudiandae tempore voluptatibus!</p>
+                <p><a class="btn btn-default" href="#" role="button">En savoir plus &raquo;</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>R&eacute;alisations</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn btn-default" href="#" role="button">En savoir plus &raquo;</a></p>
+            </div>
+
+        </div>
+
+        <hr>
+
+        <footer>
+            <p>&copy; Nicole Gingras 2015</p>
+        </footer>
+    </div> <!-- /container -->
+
+
+    <!----------------------------------
+        Fin du code html
+    ----------------------------------->
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <!-- Bootstrap Javascript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+    <script>
+        $(function() {
+            //-----------------------------------
+            // Mettre votre code javascript ici, si nécessaire
+            //-----------------------------------
+        });
+    </script>
+</body>
+</html>
